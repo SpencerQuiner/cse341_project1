68f3e474f1597238e40f2660c66f706231e3f2dc
SpencerQuiner <spencerquiner@gmail.com>
2026-03-13 12:09:05 +0000

    added Post, Put, and Delete routes, controller functions, and rest endpoints.

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -11,11 +11,58 @@ const getAll = async (req, res) => {
 
 const getSingle = async (req, res) => {
     const contactId = new ObjectId(req.params.id)
-    const result = await mongodb.getDatabase().collection('contacts').find({_id: contactId});
-    result.toArray().then((contacts) => {
+    const result = await mongodb.getDatabase().collection('contacts').findOne({_id: contactId});
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(contacts[0])
-    });   
+        res.status(200).json(contacts[0])  
 };
 
-module.exports = {getAll, getSingle};
+const createContact = async(req, res) => {
+    const contact = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthDate
+    };
+    const response = await mongodb.getDatabase().collection('contacts').insertOne(contact);
+    if (response.acknowledged) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'some error occurred while creating the contact.');
+    }
+};
+
+const updateContact = async(req, res) => {
+    const contactId = new ObjectId(req.params.id);
+    const contact = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthDate
+    };
+    const response = await mongodb.getDatabase().collection('contacts').replaceOne({_id: contactId}, contact);
+    if (response.modifiedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'some error occurred while updating the contact.');
+    }
+};
+
+const deleteContact = async(req, res) => {
+    const contactId = new ObjectId(req.params.id);
+    const response = await mongodb.getDatabase().collection('contacts').deleteOne({_id: contactId}, true);
+    if (response.deletedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'some error occurred while deleting the contact.');
+    }
+};
+
+module.exports = {
+    getAll, 
+    getSingle, 
+    createContact, 
+    updateContact, 
+    deleteContact
+};
